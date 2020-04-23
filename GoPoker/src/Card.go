02989e8528db1a1a6031c7b244f679a591a1390a
Travis <trvaughn@indiana.edu>
2020-04-22 19:17:38 -0500

    Split up logic to separate files. Setup basic REST API.

---
+++ GoPoker/src/Card.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"strings"
+	// "encoding/json"
+)
+//*** Card Logic | TODO: Move to own file ***//
+
+
+type CardSuit string
+const (
+	Clubs CardSuit = "c"
+	Diamonds CardSuit = "d"
+	Hearts CardSuit = "h"
+	Spades CardSuit = "s"
+)
+
+var CardSuitNames = map[CardSuit]string {
+	Clubs: "Clubs",
+	Diamonds : "Diamonds",
+	Hearts : "Hearts",
+	Spades : "Spades",
+}
+
+
+
+// type CardSuit struct {
+// 	Name string 
+// 	ShortName string
+// }
+
+// var (
+// 	Clubs = CardSuit{"Clubs", "c"}
+// 	Diamonds = CardSuit{"Diamonds", "d"}
+// 	Hearts = CardSuit{"Hearts", "h"}
+// 	Spades = CardSuit{"Spades", "s"}
+// )
+
+// Numerical values make comparison easier
+type CardValue int
+const (
+	Cv2 CardValue = 2 + iota
+	Cv3
+	Cv4
+	Cv5
+	Cv6
+	Cv7
+	Cv8
+	Cv9
+	CvT
+	CvJ
+	CvQ
+	CvK
+	CvA 
+)
+
+type CardFace struct {
+	Name string
+	ShortName string
+}
+var CardFaces = map[CardValue]CardFace {
+	Cv2: CardFace{"Two", "2"},
+	Cv3 : CardFace{"Three", "3"},
+	Cv4 : CardFace{"Four", "4"},
+	Cv5 : CardFace{"Five", "5"},
+	Cv6 : CardFace{"Six", "6"},
+	Cv7 : CardFace{"Seven", "7"},
+	Cv8 : CardFace{"Eight", "8"},
+	Cv9 : CardFace{"Nine", "9"},
+	CvT : CardFace{"Ten", "T"},
+	CvJ : CardFace{"Jack", "J"},
+	CvQ : CardFace{"Queen", "Q"},
+	CvK : CardFace{"King", "K"},
+	CvA : CardFace{"Ace", "A"},
+}
+
+
+type Card struct {
+	Value CardValue `json: "Value"`
+	Suit CardSuit `json: "Suit"`
+}
+
+// Treat as calc field to to use Card in REST
+func (this *Card) Face() CardFace {
+	return CardFaces[this.Value]
+}
+
+func StringifyCards(cards []*Card) string{
+	var str strings.Builder 
+	str.WriteString(cards[0].ShortName())
+
+	for _,card := range cards[1:] {
+		str.WriteString(" ")
+		str.WriteString(card.ShortName())
+	}
+
+	return str.String()
+}
+
+func newCard(cv CardValue, cs CardSuit) *Card {
+	return &Card{cv, cs}
+}
+
+func (this *Card) ShortName() string {
+	return this.Face().ShortName + string(this.Suit)
+}
+
+func (this *Card) IsInvalidCard() bool {
+	return this.Value == 0 || string(this.Suit) == ""
+}
+
+func (this *Card) SameAs(c *Card) bool {
+	return this.ShortName() == c.ShortName()
+}
+
+func (this *Card) Equals(c *Card) bool {
+	return this.Value == c.Value
+}
+func (this *Card) GreaterThan(c *Card) bool {
+	return this.Value > c.Value
+}
+func (this *Card) GreaterThanOrEqual(c *Card) bool {
+	return this.Value >= c.Value
+}
+func (this *Card) LessThan(c *Card) bool {
+	return this.Value < c.Value
+}
+func (this *Card) LessThanOrEqual(c *Card) bool {
+	return this.Value <= c.Value
+}
+
+//*** End Card Logic ***//
---
+++ GoPoker/src/Hand.go
@@ -0,0 +1,135 @@
+package main
+
+import(
+	"fmt"
+	"sort"
+)
+//*** Hand Logic | TODO: Move to own file ***//
+
+type HandValue int 
+const (
+	HighCard HandValue = iota
+	Pair
+	TwoPairs
+	Set
+	Straight
+	Flush
+	FullHouse
+	Quads
+	StraightFlush
+	RoyalFlush
+)
+
+type HandFace string
+var HandFaces = map[HandValue]HandFace {
+	HighCard: "High Card",
+	Pair : "Pair",
+	TwoPairs : "Two Pairs",
+	Set : "Three of a Kind",
+	Straight : "Straight",
+	Flush  : "Flush",
+	FullHouse  : "Full House",
+	Quads  : "Four of a Kind",
+	StraightFlush  : "Straight Flush",
+	RoyalFlush : "Royal Flush",
+}
+
+type Hand struct {
+	Value HandValue
+	Face HandFace
+	Cards []*Card
+}
+
+
+func IsInvalidHand(orderedCards []*Card) bool {
+	if len(orderedCards) != 5 { return true }
+
+	prevCard := orderedCards[0]
+	if prevCard.IsInvalidCard() { return true }
+
+	for _, card := range orderedCards[1:] {
+		if card.IsInvalidCard() { return true}
+		if card.SameAs(prevCard) { return true }
+		prevCard = card
+	}
+
+	return false
+}
+
+func newHand(cards []*Card) *Hand {
+	// Order cards for illogical check and straight matching
+	sort.Slice(cards, func(i, j int) bool { 
+		return cards[i].Value < cards[j].Value 
+	})
+
+	if IsInvalidHand(cards) {
+		fmt.Printf("Invalid hand input! %s\n", StringifyCards(cards)); 
+		return nil;
+	}
+
+	var HandValue HandValue = newHandValue(cards)
+	return &Hand{HandValue, HandFaces[HandValue], cards}
+}
+
+// Due to frequency, sacrificing readability for optimization
+func newHandValue(orderedCards []*Card) HandValue {
+	var result HandValue = Pair
+
+	// Map by card value for quicker matching
+	// Impossible to have > 4 of same card value
+	var cardValues = make(map[CardValue][]*Card)
+	for _, card := range orderedCards { 
+		cardValues[card.Value] = append(cardValues[card.Value], card)
+	}
+
+	switch len(cardValues) {
+	case 5:
+		var isFlush bool = true;
+		var isStraight bool = true;
+		var prevCard *Card = orderedCards[0]
+		for i, card := range orderedCards[1:] {
+			if card.Suit != prevCard.Suit { isFlush = false;}
+			if card.Value - 1 != prevCard.Value { 
+				// Edge case of ace-5 straight
+				if !(i == 3 && card.Value == CvA && orderedCards[0].Value == Cv2) {
+					isStraight = false; 
+				}
+			}
+			prevCard = card
+		}
+		switch isFlush {
+		case true:
+			switch isStraight {
+			case true:
+				if (orderedCards[0].Value == CvT) {
+					result = RoyalFlush
+				} else {
+					result = StraightFlush
+				}
+			case false:
+				result = Flush
+			}
+		case false:
+			if (isStraight) {
+				result = Straight
+			} else {
+				result = HighCard
+			}
+		}
+	case 4:
+		result = Pair
+	case 3:
+		result = TwoPairs
+		for _, cardValue := range cardValues {
+			if len(cardValue) > 2 { result = Set }
+		}
+	case 2:
+		result = FullHouse
+		for _, cardValue := range cardValues {
+			if len(cardValue) > 3 { result = Quads }
+		}
+	}
+	
+	return result	
+}
+//*** End Hand Logic ***//
